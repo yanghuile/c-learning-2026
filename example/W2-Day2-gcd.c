51f47485c917170c449b3ed790be2419dc2dfe3c
yanghuile <3412721069qq.com>
2026-09-15 22:15:22 +0800

    枚举求最大公约数

---
+++ example/W2-Day2-gcd.c
@@ -0,0 +1,31 @@
+#include <stdio.h>
+
+int main()
+{
+    int a,b;
+    int min;
+
+    scanf("%d %d",&a,&b);
+    if(a<b)
+    {
+        min=a;
+    }
+    else
+    {
+        min=b;
+    }
+    int ret;
+    for(int i=1;i<min;i++)
+    {
+        if(a%i==0)
+        {
+            if(b%i==0)
+            {
+                ret=i;
+            }
+        }
+    }
+    printf("%d和%d的最大公约数是:%d",a,b,ret);
+
+    return 0;
+}
