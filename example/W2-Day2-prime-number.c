77deb9ef6f7db9d5e068d785717db63e0d44799d
yanghuile <3412721069qq.com>
2026-09-15 19:57:55 +0800

    用for判断素数

---
+++ example/W2-Day2-prime-number.c
@@ -0,0 +1,28 @@
+#include <stdio.h>
+
+int main()
+{
+    int x;
+    scanf("%d",&x);
+
+    int isPrime=1;    //设标志位
+
+    for(int i=2;i<x;i++)
+    {
+        if(x%i==0)
+        {
+            isPrime=0;
+            //break;
+        }
+    }
+    if(isPrime==1)
+    {
+        printf("这个数是素数");
+    }
+    else
+    {
+         printf("这个数不是素数");
+    }
+
+    return 0;
+}
