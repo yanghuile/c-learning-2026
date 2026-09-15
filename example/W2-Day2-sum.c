685fb8d185a2ac83aae71309e458e941bb765817
yanghuile <3412721069qq.com>
2026-09-15 20:53:54 +0800

    交错符号分数求和

--- example/W2-Day2-sum.c
+++ example/W2-Day2-sum.c
@@ -3,13 +3,15 @@
 int main()
 {
     double sum=0.0;
+    double sign=1.0;
     int n;
     scanf("%d",&n);
 
-    //求f(n)=1+1/1+1/2+1/3+…1/n
+    //求f(n)=1-1/1+1/2-1/3+…+1/n
     for(int i=1;i<=n;i++)
     {
-        sum+=1.0/i;
+        sum+=sign/i;
+        sign=-sign;
     }
     printf("f(%d)=%f\n",n,sum);
 
