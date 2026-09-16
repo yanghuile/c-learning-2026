6b9613ab4b4cfeb0394162521434c93626132289
yanghuile <3412721069qq.com>
2026-09-16 19:23:41 +0800

    练习数组求平均数并找出大于平均数的数

---
+++ example/W2-Day3-array-average.c
@@ -0,0 +1,36 @@
+#include <stdio.h>
+
+int main()
+{
+    int x;
+    int cnt=0;
+    double sum=0;
+    int number[100];
+    scanf("%d",&x);
+
+    while(x!=-1)
+    {
+        number[cnt]=x;
+        sum+=x;
+        cnt++;
+        scanf("%d",&x);
+    }
+
+    if(cnt==0)
+    {
+        printf("你未输入任何有效数字\n");
+    }
+    else
+    {
+        printf("平均数是%.2f:\n",sum/cnt);
+        for(int i=0;i<cnt;i++)
+        {
+            if(number[i]>sum/cnt)
+            {
+                printf("%d\n",number[i]);
+            }
+        }
+    }
+
+    return 0;
+}
