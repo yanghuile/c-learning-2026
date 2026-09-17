a26543edbcf287e6a551ea714c2df5a17c933265
yanghuile <3412721069qq.com>
2026-09-17 13:14:41 +0800

    AI优化逻辑

--- example/W2-Day4-array-maxmin.c
+++ example/W2-Day4-array-maxmin.c
@@ -4,17 +4,27 @@ int main()
 {
     int arr[100];
     int diff=0;
-    int max,min;
     int n;
     scanf("%d",&n);
+
+    //判断n是否在有效范围内
+    if(n<1||n>100)
+    {
+        printf("输入数量超出范围\n");
+        return 0;
+    }
 
     for(int j=0;j<n;j++)
     {
         scanf("%d",&arr[j]);
     }
-    max=arr[0];
-    min=arr[0];
-    for(int i=0;i<n;i++)
+
+    //使用arr[0]初始化最大值和最小值
+    int max=arr[0];
+    int min=arr[0];
+
+    //遍历数组做判断
+    for(int i=0;i<n;i++)  //可以直接从 i=1 开始，因为 i=0 就是初始值
     {
         if(arr[i]>max)
         {
