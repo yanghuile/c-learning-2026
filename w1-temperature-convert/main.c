0b001cd556e43a785de948c8785006179c8bc4a7
yanghuile <3412721069qq.com>
2026-09-19 16:12:19 +0800

    第一周：温度转换

--- w1-temperature-convert/main.c
+++ w1-temperature-convert/main.c
@@ -4,11 +4,11 @@ int main()
 {
 	float c,f;
 	printf("请输入摄氏度：");
-	scanf_s("%f", &c);
+	scanf("%f", &c);
 	f = c * (9.0 / 5.0) + 32;
 	printf("对应的华氏度为：%.2f°F\n", f);
 	printf("请输入华氏度：");
-	scanf_s("%f", &f);
+	scanf("%f", &f);
 	c = (f - 32) * (5.0 / 9.0);
 	printf("对应的摄氏度为：%.2f°C", c);
 	
